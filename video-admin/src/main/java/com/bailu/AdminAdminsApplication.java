0afb44d2dfca2e13ceeb755c63ab42fe321e0921
wangyunpeng <wangyunpeng@zhangwen.cn>
2021-07-30 16:26:06 +0800

    init

---
+++ video-admin/src/main/java/com/bailu/AdminAdminsApplication.java
@@ -0,0 +1,17 @@
+package com.bailu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：wangyunpeng@zhangwen.com
+ * @date ：Created in 2021/7/30 15:32
+ * @description：启动类
+ * @version:
+ */
+@SpringBootApplication
+public class AdminAdminsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminAdminsApplication.class,args);
+    }
+}
---
+++ video-category/src/main/java/com/bailu/AdminCategoryApplication.java
@@ -0,0 +1,17 @@
+package com.bailu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：wangyunpeng@zhangwen.com
+ * @date ：Created in 2021/7/30 15:35
+ * @description：启动类
+ * @version:
+ */
+@SpringBootApplication
+public class AdminCategoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminCategoryApplication.class,args);
+    }
+}
